62fb3257032e7cc74bd2c1d343c3771841855f90
gulsher <ahmed.gulsher@gmail.com>
2023-04-07 22:03:21 +0530

    string in python

---
+++ app.py
@@ -0,0 +1,12 @@
+character_name = "John Smith"
+character_age = 21;
+print(character_name, "\n" , character_age)
+
+#string
+print(character_name.upper() + " is cool")
+print(character_name.upper().isupper())
+print(len(character_name))
+print(character_name[2])
+print(character_name.index("h")); 
+print(character_name.index("th"));
+print(character_name.replace("Smith", "Wick"))
