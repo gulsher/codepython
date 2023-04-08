019974d7b2a87946335ef60fbe6a7e57daa0a199
gulsher <ahmed.gulsher@gmail.com>
2023-04-08 18:08:11 +0530

    numbers in python

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from math import *
+
 character_name = "John Smith"
 character_age = 21;
 print(character_name, "\n" , character_age)
@@ -10,3 +12,22 @@ print(character_name[2])
 print(character_name.index("h")); 
 print(character_name.index("th"));
 print(character_name.replace("Smith", "Wick"))
+
+#numbers in python
+
+print(10 + 20)
+print(10 - 20)
+print(10 * 20)
+print(10 / 20)
+print(10 // 20)
+my_num = 5
+print(str(my_num) + " is cool")
+my_num1 = -10
+print(abs(my_num1))
+print(pow(3,2))
+print(max(2,3))
+print(min(2,3))
+print(round(3.6))
+print(floor(3.6))
+print(ceil(3.6))
+print(sqrt(9))
