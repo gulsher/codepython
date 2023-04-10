974f275b7f1cb746907c764e305d2747363df37f
gulsher <ahmed.gulsher@gmail.com>
2023-04-10 21:28:31 +0530

    madlib game in python

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celeb = input("Enter a celeb: ")
+
+
+print("roses are " + color)
+print(plural_noun + " are blue")
+print("i love " + celeb)
