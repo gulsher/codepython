d7c02b72dcbadd1a3ee79f31dd9e091b7593e169
gulsher <ahmed.gulsher@gmail.com>
2023-04-18 01:19:59 +0530

    for loops in python

---
+++ forloop.py
@@ -0,0 +1,11 @@
+
+friends =["jim","john","wick"]
+for index in range(3,10):
+    print(index)
+
+
+for friend in friends:
+    print(friend)
+
+for index in range(len(friends)):
+    print(friends[index])
