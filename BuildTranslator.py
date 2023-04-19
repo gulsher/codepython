8c383926da65ef90786dbeaa822c8345e4691a2f
gulsher <ahmed.gulsher@gmail.com>
2023-04-19 15:23:10 +0530

    Translator game in python

---
+++ BuildTranslator.py
@@ -0,0 +1,14 @@
+def translate(texts):
+    translation=""
+    for letter in texts:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return  translation
+
+
+print(translate("On"))
