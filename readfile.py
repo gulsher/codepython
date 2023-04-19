0d9a5ff252735cae850aff9f5e3988dcb775b622
gulsher <ahmed.gulsher@gmail.com>
2023-04-19 18:02:03 +0530

    write files in python

--- readfile.py
+++ readfile.py
@@ -6,3 +6,11 @@ for employee in employee_file.readlines():
     print(employee)
 
 employee_file.close()
+
+
+#write file
+employee_files = open('NewEmp.txt', "w")
+
+employee_files.write("\nToby - Intern")
+
+employee_files.close()
